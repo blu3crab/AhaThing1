f72357602552ebf1b7044312763c2f031cb72f99
matucker <matucker@adaptivehandyapps.com>
2017-02-10 08:51:38 -0500

    Firebase Auth - email, google, anon

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,7 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+# firebase
+-keepattributes Signature
+-keepattributes *Annotation*
--- app/src/main/java/com/adaptivehandyapps/ahathing/MainActivity.java
+++ app/src/main/java/com/adaptivehandyapps/ahathing/MainActivity.java
@@ -2,7 +2,10 @@ package com.adaptivehandyapps.ahathing;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -18,6 +21,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.adaptivehandyapps.ahathing.dal.PlayProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 ///////////////////////////////////////////////////////////////////////////
 public class MainActivity extends AppCompatActivity
@@ -28,6 +33,10 @@ public class MainActivity extends AppCompatActivity
     private boolean mVacating = false;
 
     PlayProvider mPlayProvider;
+
+    // Firebase auth
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     ///////////////////////////////////////////////////////////////////////////
     @Override
@@ -63,6 +72,23 @@ public class MainActivity extends AppCompatActivity
         // update the main content with stage
         int contentId = R.layout.content_stage;
         replaceFragment(contentId);
+
+        // Firebase auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...TODO?
+            }
+        };
     }
     ///////////////////////////////////////////////////////////////////////////
     // getters/setters
@@ -94,7 +120,40 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_whoami) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user != null) {
+                // Name, email address, and profile photo Url
+                String name = user.getDisplayName();
+                String email = user.getEmail();
+                Uri photoUrl = user.getPhotoUrl();
+
+                // The user's ID, unique to the Firebase project. Do NOT use this value to
+                // authenticate with your backend server, if you have one. Use
+                // FirebaseUser.getToken() instead.
+                String uid = user.getUid();
+                Log.d(TAG, name + ", " + email + ", " + uid);
+                Toast.makeText(this, name + ", " + email + ", " + uid, Toast.LENGTH_LONG).show();
+            }
+            else {
+                Log.d(TAG, "No FirebaseUser found.");
+                Toast.makeText(this, "Please signin. No FirebaseUser found.", Toast.LENGTH_LONG).show();
+            }
+            return true;
+        }
+        else if (id == R.id.action_anonauth) {
+            startActivity(new Intent(this, AnonymousAuthActivity.class));
+            return true;
+        }
+        else if (id == R.id.action_emailauth) {
+            startActivity(new Intent(this, EmailPasswordActivity.class));
+            return true;
+        }
+        else if (id == R.id.action_googleauth) {
+            startActivity(new Intent(this, GoogleSignInActivity.class));
+            return true;
+        }
+        else if (id == R.id.action_settings) {
             return true;
         }
 
@@ -137,6 +196,15 @@ public class MainActivity extends AppCompatActivity
     ///////////////////////////////////////////////////////////////////////////////////////////
     // lifecycle methods
     @Override
+    protected void onStart() {
+        super.onStart();
+        mVacating = false;
+        Log.v(TAG, "onStart");
+        // Firebase
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         mVacating = false;
@@ -155,6 +223,10 @@ public class MainActivity extends AppCompatActivity
         mVacating = true;
         super.onStop();
         Log.v(TAG, "onStop");
+        // Firebase
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
